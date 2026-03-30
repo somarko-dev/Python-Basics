df779f0e1e4d37a354ed29853b353cf7d80dd31b
somarko-dev <somarkosarkarwork@gmail.com>
2026-03-30 17:01:07 +0530

    Variables

---
+++ Variables_basics.py
@@ -0,0 +1,11 @@
+# Variables in Python
+
+name = "Somarko"
+age = 18
+height = 5.6
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
